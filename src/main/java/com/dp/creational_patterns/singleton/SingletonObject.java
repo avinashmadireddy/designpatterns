274cf1f05b62c9d4d41d5ce2e6f113f727a68789
Avinash Madireddy <avinash@seqnc.com>
2019-03-11 19:36:13 +0530

    singleton

--- src/main/java/com/dp/creational_patterns/singleton/SingletonObject.java
+++ src/main/java/com/dp/creational_patterns/singleton/SingletonObject.java
@@ -1,12 +1,27 @@
 package com.dp.creational_patterns.singleton;
 
 public class SingletonObject {
-	private static SingletonObject instance = new SingletonObject();
-
+	// volatile : thread safe. not to make any changes inside the JVM
+	private static volatile SingletonObject instance = null;// new
+															// SingletonObject();
+	
+	public SingletonObject() {
+		// thread safe
+		if (instance != null)
+			throw new RuntimeException("use getInstance() method");
+	}
+	
 	public static SingletonObject getInstance() {
+		if (instance == null) {// lazy loading;
+			synchronized (SingletonObject.class) {// thread safe
+				if (instance == null) {
+					instance = new SingletonObject();
+				}
+			}
+		}
 		return instance;
 	}
-
+	
 	public void showMessage() {
 		System.out.println("Singleton");
 	}
--- src/main/java/com/dp/creational_patterns/singleton/SingletonPattern.java
+++ src/main/java/com/dp/creational_patterns/singleton/SingletonPattern.java
@@ -4,7 +4,16 @@ public class SingletonPattern {
 	public static void main(String[] args) {
 		SingletonObject instance = SingletonObject.getInstance();
 		instance.showMessage();
+		//
+
+		Runtime runtime = Runtime.getRuntime();
+//		
+
+		Runtime anotherRuntime = Runtime.getRuntime();
+//		
+		if (runtime == anotherRuntime)
+			System.out.println("same instances");
 	}
-	
-	//singleton, prototype, request, session, global-session : spring prototypes
+
+	// singleton, prototype, request, session, global-session : spring prototypes
 }
